a0e549a03eb04c1dc28512648550aa0cfa9e63c1
Igor <igor@art-storm.com>
2021-10-03 22:35:30 +0300

    update librries

--- vars/helpers.groovy
+++ vars/helpers.groovy
@@ -1,5 +1,32 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-    echo "Hello, ${name}."
+def getImageName(branch = 'dev') {
+    def imagename = "crappjavatest/dev-app-java"
+    if (branch == 'release') {
+        imagename = "crappjavatest/rc-app-java"
+    }
+
+    return imagename
+}
+
+def getEnvironment(branch = 'dev') {
+    def env = "dev"
+    if (branch == 'release') {
+        env = "staging"
+    }
+
+    return env
+}
+
+def execTerraform(command) {
+    withCredentials([azureServicePrincipal(
+            credentialsId: 'azure-service-principle',
+            subscriptionIdVariable: 'ARM_SUBSCRIPTION_ID',
+            clientIdVariable: 'ARM_CLIENT_ID',
+            clientSecretVariable: 'ARM_CLIENT_SECRET',
+            tenantIdVariable: 'ARM_TENANT_ID'
+    )])
+            {
+                sh command
+            }
 }
